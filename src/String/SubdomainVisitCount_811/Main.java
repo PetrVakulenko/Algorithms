7aebbb93b6b2437751f907a939c310745a502b09
PetrVakulenko <vakulenkopetya@gmail.com>
2018-04-13 11:27:17 +0300

    Subdomain Visit count algorithm init

---
+++ src/String/SubdomainVisitCount_811/Main.java
@@ -0,0 +1,11 @@
+package String.SubdomainVisitCount_811;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        //String arr[] = {"9001 discuss.leetcode.com"};
+        String arr[] = {"9001 discuss.leetcode.com", "9001 leetcode.com", "9001 com"};
+        s.subdomainVisits(arr);
+        //System.out.println();
+    }
+}
