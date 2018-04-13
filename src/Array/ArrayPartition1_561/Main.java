d18daff49974e360d2c0ad8059b113a1fa9e4693
PetrVakulenko <vakulenkopetya@gmail.com>
2018-04-13 10:22:49 +0300

    Init arraypartition I

---
+++ src/Array/ArrayPartition1_561/Main.java
@@ -0,0 +1,11 @@
+package Array.ArrayPartition1_561;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int arr[] = {1,4,3,2};
+        System.out.println(s.arrayPairSum(arr));
+    }
+}
