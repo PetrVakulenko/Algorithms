06248486b67d999836f05907feb9f3451b047e9a
PetrVakulenko <vakulenkopetya@gmail.com>
2018-04-17 13:38:05 +0300

    Init algorithm - Baseball game

--- src/Others/FizzBuzz_412/Solution.java
+++ src/Others/FizzBuzz_412/Solution.java
@@ -13,7 +13,7 @@ import java.util.List;
 
  Example:
  n = 15,
- 
+
  Return:
  [ "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz" ]
  */
---
+++ src/Structures/BaseballGame/Main.java
@@ -0,0 +1,10 @@
+package Structures.BaseballGame;
+
+public class Main {
+    public static void main(String[] args){
+        Solution s = new Solution();
+        //String example[] = {"5","2","C","D","+"};
+        String example[] = {"5","-2","4","C","D","9","+","+"};
+        System.out.println(s.calPoints(example));
+    }
+}
