8101009ec7ca9e9f4a181e79ed5759a9dde9ea8b
PetrVakulenko <vakulenkopetya@gmail.com>
2018-04-18 15:44:17 +0300

    Init algorithm - Island Perimeter

---
+++ src/Array/IslandPerimeter_463/Main.java
@@ -0,0 +1,9 @@
+package Array.IslandPerimeter_463;
+
+public class Main {
+    public static void main(String[] argv){
+        Solution s = new Solution();
+        int[][] example = {{0,1,0,0},{1,1,1,0},{0,1,0,0},{1,1,0,0}};
+        System.out.println(s.islandPerimeter(example));
+    }
+}
