1a96c71f7fdf29ce3101fb12ecd737214ff0c091
PetrVakulenko <vakulenkopetya@gmail.com>
2018-11-06 11:40:27 +0300

    [LeetCode] Fixed printResult function

--- src/Utils/PrintResult.java
+++ src/Utils/PrintResult.java
@@ -15,7 +15,7 @@ public class PrintResult {
     public static void printArray(int[] arr)
     {
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i] + " ");
+            System.out.print(arr[i] + " ");
         }
     }
 }
