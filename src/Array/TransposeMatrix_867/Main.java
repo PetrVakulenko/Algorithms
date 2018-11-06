6207edd55a416ce95b8f479dab350768c9569c8c
PetrVakulenko <vakulenkopetya@gmail.com>
2018-11-06 17:02:48 +0300

    [LeetCode] Added Transpose Matrix solution

---
+++ src/Array/TransposeMatrix_867/Main.java
@@ -0,0 +1,11 @@
+package Array.TransposeMatrix_867;
+
+public class Main {
+    public static void main(String[] argv){
+        Solution s = new Solution();
+        //int[][] matrix = {{11,74,0,93},{40,11,74,7}};//{{0,1,2,3},{1,0,1,2},{2,1,0,1},{3,2,1,0}};
+        //int[][] matrix = {{41,45},{81,41},{73,81},{47,73},{0,47},{79,76}};
+        int[][] matrix = {{11,74,0,93},{40,11,74,7}};
+        Utils.PrintResult.printMatrix(s.transpose(matrix));
+    }
+}
