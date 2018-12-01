f67058f9582496eb549306a28f8c2b18b62016f7
PetrVakulenko <vakulenkopetya@gmail.com>
2018-12-01 21:06:13 +0200

    [LeetCode] Task with eitht houses

---
+++ src/Array/EightHouses/Main.java
@@ -0,0 +1,9 @@
+package Array.DistributeCandies_575;
+
+public class Main {
+    public static void main(String[] argv){
+        Solution s = new Solution();
+        int[] candies = {1,1,2,2,3,3};
+        System.out.println(s.distributeCandies(candies));
+    }
+}
