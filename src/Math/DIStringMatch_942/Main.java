7e6e404464bb3c25d6bda71f3f6f7c34b313eb5f
PetrVakulenko <vakulenkopetya@gmail.com>
2018-12-03 00:21:41 +0200

    [LeetCode] Solution for `DI String Match`

---
+++ src/Math/DIStringMatch_942/Main.java
@@ -0,0 +1,9 @@
+package Math.DIStringMatch_942;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String diString = "D";
+        Utils.PrintResult.printArray(s.diStringMatch(diString));
+    }
+}
