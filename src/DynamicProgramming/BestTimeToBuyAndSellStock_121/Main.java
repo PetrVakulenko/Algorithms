96dd115aa004a765d249dc527f7dbbe829dbccba
PetrVakulenko <vakulenkopetya@gmail.com>
2018-12-02 13:24:44 +0200

    [LeetCode] Solution for `Best Time to Buy and Sell Stock`

---
+++ src/DynamicProgramming/BestTimeToBuyAndSellStock_121/Main.java
@@ -0,0 +1,9 @@
+package DynamicProgramming.BestTimeToBuyAndSellStock_121;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] prices = {2,1,2,1,0,1,2};
+        System.out.println(s.maxProfit(prices));
+    }
+}
