c2bb4efa4ae0acd1e262f6ae79987042009d423f
PetrVakulenko <vakulenkopetya@gmail.com>
2018-12-05 18:12:23 +0200

    [LeetCode] Solution for `Rectangele overlap`

---
+++ src/Math/RectangleOverlap_836/Main.java
@@ -0,0 +1,10 @@
+package Math.RectangleOverlap_836;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] A = {0,0,1,1};
+        int[] K = {1,0,2,1};
+        System.out.println(s.isRectangleOverlap(A, K));
+    }
+}
