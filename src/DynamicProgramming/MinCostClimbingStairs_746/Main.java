f8a95f95a879a82fb3e8c95f42535837168054c6
PetrVakulenko <vakulenkopetya@gmail.com>
2018-12-23 23:51:51 +0200

    [LeetCode] Solution for `Min cost climbing stairs`

---
+++ src/DynamicProgramming/MinCostClimbingStairs_746/Main.java
@@ -0,0 +1,10 @@
+package DynamicProgramming.MinCostClimbingStairs_746;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+//        int[] nums = {1, 100, 1, 1, 1, 100, 1, 1, 100, 1};
+        int[] nums = {0,1,2,2};
+        System.out.println(s.minCostClimbingStairs(nums));
+    }
+}
