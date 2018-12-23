bd00436015e8c03103adb29cf673e55aa2b30f2e
PetrVakulenko <vakulenkopetya@gmail.com>
2018-12-23 17:20:49 +0200

    [LeetCode] Solution for `Max increase to keep city skyline`

---
+++ src/Array/MaxIncreaseToKeepCitySkyline__807/Main.java
@@ -0,0 +1,9 @@
+package Array.MaxIncreaseToKeepCitySkyline__807;
+
+public class Main {
+    public static void main(String[] argv){
+        Solution s = new Solution();
+        int[][] matrix = {{3,0,8,4},{2,4,5,7},{9,2,6,3},{0,3,1,0}};
+        System.out.println(s.maxIncreaseKeepingSkyline(matrix));
+    }
+}
