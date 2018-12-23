7ec5918cb9c2920d5142ea28ef31a62cd5c20a7e
PetrVakulenko <vakulenkopetya@gmail.com>
2018-12-24 01:28:47 +0200

    [LeetCode] Solution for `Climbing stairs`

---
+++ src/DynamicProgramming/ClimbingStairs__70/Main.java
@@ -0,0 +1,9 @@
+package DynamicProgramming.ClimbingStairs__70;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int stairs = 7;
+        System.out.println(s.climbStairs(stairs));
+    }
+}
