6369b010163c5e3c2c0a7030fbf9ce4b6af0554a
PetrVakulenko <vakulenkopetya@gmail.com>
2018-12-24 13:37:32 +0200

    [LeetCode] Solution for `N-repeated element in size 2n array`

---
+++ src/HashTable/N_RepeatedElementInSize2NArray_961/Main.java
@@ -0,0 +1,9 @@
+package HashTable.N_RepeatedElementInSize2NArray_961;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] input = {5,1,5,2,5,3,5,4};
+        System.out.println(s.repeatedNTimes(input));
+    }
+}
---
+++ src/HashTable/N_RepeatedElementInSize2NArray_961/Solution.java
@@ -0,0 +1,41 @@
+package HashTable.N_RepeatedElementInSize2NArray_961;
+
+import java.util.Set;
+import java.util.HashSet;
+
+/**
+ 961. N-Repeated Element in Size 2N Array
+ https://leetcode.com/problems/n-repeated-element-in-size-2n-array/
+
+ In a array A of size 2N, there are N+1 unique elements, and exactly one of these elements is repeated N times.
+ Return the element repeated N times.
+
+ Example 1:
+ Input: [1,2,3,3]
+ Output: 3
+
+ Example 2:
+ Input: [2,1,2,5,3,2]
+ Output: 2
+
+ Example 3:
+ Input: [5,1,5,2,5,3,5,4]
+ Output: 5
+
+ Note:
+ 4 <= A.length <= 10000
+ 0 <= A[i] < 10000
+ A.length is even
+
+ */
+
+public class Solution {
+    public int repeatedNTimes(int[] A) {
+        Set<Integer> vis = new HashSet<>();
+        for (int num : A) {
+            if (vis.contains(num)) return num;
+            vis.add(num);
+        }
+        return -1;
+    }
+}
