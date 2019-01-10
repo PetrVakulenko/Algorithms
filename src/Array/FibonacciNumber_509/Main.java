8d7af1b0838ce44666e4ccfa6719e80ed89cdcba
PetrVakulenko <vakulenkopetya@gmail.com>
2019-01-10 21:08:25 +0200

    [LeetCode] Solution for `Fibbonacci number`

---
+++ src/Array/FibonacciNumber_509/Main.java
@@ -0,0 +1,9 @@
+package Array.FibonacciNumber_509;
+
+public class Main {
+    public static void main(String[] argv){
+        Solution s = new Solution();
+        int input = 2;
+        System.out.println(s.fib(input));
+    }
+}
