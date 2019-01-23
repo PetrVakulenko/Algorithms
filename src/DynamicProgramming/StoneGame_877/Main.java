1a851130fca42b71f9c0f2b8407cc3969a25cdef
PetrVakulenko <vakulenkopetya@gmail.com>
2019-01-23 19:17:01 +0200

    [LeetCode] Solution for `Stone game`

---
+++ src/DynamicProgramming/StoneGame_877/Main.java
@@ -0,0 +1,9 @@
+package DynamicProgramming.StoneGame_877;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {1, 100, 1, 1, 1, 100, 1, 1, 100, 1};
+        System.out.println(s.stoneGame(nums));
+    }
+}
