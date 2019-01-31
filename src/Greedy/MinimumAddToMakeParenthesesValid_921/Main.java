67d552d29df6c5a74d0301e68d1c29cecd240b8e
PetrVakulenko <vakulenkopetya@gmail.com>
2019-01-31 10:54:05 +0200

    [LeetCode] Solution for `Minimum add to make parentheses valid`

---
+++ src/Greedy/MinimumAddToMakeParenthesesValid_921/Main.java
@@ -0,0 +1,9 @@
+package Greedy.DeleteColumnsToMakeSorted_944;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String[] input = {"abcdef","uvwxyz"};
+        System.out.println(s.minDeletionSize(input));
+    }
+}
