0a064783382414ba2e7d32e4cc65b9e3bbc1389e
PetrVakulenko <vakulenkopetya@gmail.com>
2019-02-04 14:17:33 +0200

    [LeetCode] Solution for `Goat latin`

---
+++ src/String/GoatLatin_824/Main.java
@@ -0,0 +1,11 @@
+package String.GoatLatin_824;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        String A = "I speak Goat Latin";
+
+        System.out.println(s.toGoatLatin(A));
+    }
+}
---
+++ src/String/GoatLatin_824/Solution.java
@@ -0,0 +1,71 @@
+package String.GoatLatin_824;
+
+import com.sun.org.apache.xerces.internal.util.SymbolHash;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ 824. Goat Latin
+ https://leetcode.com/problems/goat-latin/
+
+ A sentence S is given, composed of words separated by spaces.
+ Each word consists of lowercase and uppercase letters only.
+ We would like to convert the sentence to "Goat Latin" (a made-up language similar to Pig Latin.)
+
+ The rules of Goat Latin are as follows:
+ If a word begins with a vowel (a, e, i, o, or u), append "ma" to the end of the word.
+ For example, the word 'apple' becomes 'applema'.
+
+ If a word begins with a consonant (i.e. not a vowel), remove the first letter and append it to the end, then add "ma".
+ For example, the word "goat" becomes "oatgma".
+
+ Add one letter 'a' to the end of each word per its word index in the sentence, starting with 1.
+ For example, the first word gets "a" added to the end, the second word gets "aa" added to the end and so on.
+ Return the final sentence representing the conversion from S to Goat Latin.
+
+ Example 1:
+ Input: "I speak Goat Latin"
+ Output: "Imaa peaksmaaa oatGmaaaa atinLmaaaaa"
+
+ Example 2:
+ Input: "The quick brown fox jumped over the lazy dog"
+ Output: "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa"
+
+ Notes:
+ S contains only uppercase, lowercase and spaces. Exactly one space between each word.
+ 1 <= S.length <= 150.
+ */
+
+public class Solution {
+    public String toGoatLatin(String S) {
+        String[] words = S.split(" ");
+        int i;
+
+        String vowels = "aeiouAEIOU", goatString = "ma";
+
+        for (i = 0; i < words.length; i++) {
+            goatString += "a";
+
+            if (vowels.indexOf(words[i].charAt(0)) != -1) {
+                words[i] += goatString;
+            } else {
+                char firstLetter = words[i].charAt(0);
+                words[i] = words[i].substring(1) + firstLetter + goatString;
+            }
+        }
+
+        StringBuilder builder = new StringBuilder();
+        i = 0;
+
+        for(String s: words) {
+            if (i++ > 0) {
+                builder.append(' ');
+            }
+            builder.append(s);
+        }
+
+        return builder.toString();
+    }
+}
