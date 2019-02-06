44c5af8754828742bec2039c02e227e6f0c6e957
PetrVakulenko <vakulenkopetya@gmail.com>
2019-02-06 16:38:41 +0200

    [LeetCode] Solution for `Largest perimeter triangle`

---
+++ src/Math/LargestPerimeterTriangle_976/Main.java
@@ -0,0 +1,9 @@
+package Math.LargestPerimeterTriangle_976;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] A = {3,2,3,4};
+        System.out.println(s.largestPerimeter(A));
+    }
+}
