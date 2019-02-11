b5894ba552478e11e3b1ec3636575f5bc251e109
PetrVakulenko <vakulenkopetya@gmail.com>
2019-02-11 20:45:22 +0200

    [LeetCode] Solution for `Reveal cards in increasing order`

---
+++ src/Array/RevealCardsInIncreasingOrder_950/Main.java
@@ -0,0 +1,11 @@
+package Array.RevealCardsInIncreasingOrder_950;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] argv){
+        Solution s = new Solution();
+        int[] input = {17,13,11,2,3,5,7};
+        System.out.println(Arrays.toString(s.deckRevealedIncreasing(input)));
+    }
+}
