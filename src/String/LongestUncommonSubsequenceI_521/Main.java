6298c319a540f388d8485ec90ab786ee365480ab
PetrVakulenko <vakulenkopetya@gmail.com>
2019-02-12 11:52:47 +0200

    [LeetCode] Longest uncommon subsequence`

---
+++ src/String/LongestUncommonSubsequenceI_521/Main.java
@@ -0,0 +1,9 @@
+package String.LongestUncommonSubsequenceI_521;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String word1 = "aba", word2 = "bab";
+        System.out.println(s.findLUSlength(word1, word2));
+    }
+}
