d06a62aa7c5a9bfb12b8146ab6654004d28cd59e
PetrVakulenko <vakulenkopetya@gmail.com>
2019-02-12 11:57:04 +0200

    [LeetCode] Solution for `Nim game`

---
+++ src/Math/NimGame_292/Main.java
@@ -0,0 +1,9 @@
+package Math.NimGame_292;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(s.canWinNim(4));
+    }
+}
