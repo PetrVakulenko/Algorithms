804868e327ab9f196a145533ac24b87caa38d217
PetrVakulenko <vakulenkopetya@gmail.com>
2019-02-12 14:36:20 +0200

    [LeetCode] Solution for `Fair candy swap`

---
+++ src/Array/FairCandySwap_888/Main.java
@@ -0,0 +1,9 @@
+package Array.FairCandySwap_888;
+
+public class Main {
+    public static void main(String[] argv){
+        Solution s = new Solution();
+        int[] A = {1,1}, B = {2,2};
+        Utils.PrintResult.printArray(s.fairCandySwap(A, B));
+    }
+}
