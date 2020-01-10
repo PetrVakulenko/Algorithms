559f6e00bd3053922fbf1008e15b9a1bf6746db6
PetrVakulenko <vakulenkopetya@gmail.com>
2020-01-10 10:09:15 +0200

    Solution for Subtract the product and sum of digits of an integer

---
+++ src/Math/SubtractTheProductAndSumOfDigitsOfAnInteger_1281/Main.java
@@ -0,0 +1,11 @@
+package Math.SubtractTheProductAndSumOfDigitsOfAnInteger_1281;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int test = 12345;
+
+        System.out.println(s.subtractProductAndSum(test));
+    }
+}
