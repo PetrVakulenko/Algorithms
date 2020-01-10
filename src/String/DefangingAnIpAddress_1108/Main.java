d356913fabba515c912b75c2d93de531e617a13e
PetrVakulenko <vakulenkopetya@gmail.com>
2020-01-10 09:57:02 +0200

    Solution for Defanging an ip address

---
+++ src/String/DefangingAnIpAddress_1108/Main.java
@@ -0,0 +1,11 @@
+package String.DefangingAnIpAddress_1108;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        String A = "1.1.1.1";
+
+        System.out.println(s.defangIPaddr(A));
+    }
+}
