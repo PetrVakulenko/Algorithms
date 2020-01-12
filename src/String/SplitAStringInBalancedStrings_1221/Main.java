7ef43d41455443b1b967dceeaad026b02f0ef9c2
PetrVakulenko <vakulenkopetya@gmail.com>
2020-01-12 20:47:37 +0200

    Solution for `Split a string in balanced strings`

---
+++ src/String/SplitAStringInBalancedStrings_1221/Main.java
@@ -0,0 +1,10 @@
+package String.SplitAStringInBalancedStrings_1221;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String S = "LRLRLRLRLRLLLRRR";
+
+        System.out.println(s.balancedStringSplit(S));
+    }
+}
