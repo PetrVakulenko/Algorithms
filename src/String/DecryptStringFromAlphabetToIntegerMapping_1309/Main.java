20344d16f759c49a5fd97695c53a016c1f0ea217
PetrVakulenko <vakulenkopetya@gmail.com>
2020-01-13 20:50:46 +0200

    Solution for `Decrypt string from alphabet to integer mapping`

---
+++ src/String/DecryptStringFromAlphabetToIntegerMapping_1309/Main.java
@@ -0,0 +1,11 @@
+package String.DecryptStringFromAlphabetToIntegerMapping_1309;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        String A = "10#11#12";
+
+        System.out.println(s.freqAlphabets(A));
+    }
+}
