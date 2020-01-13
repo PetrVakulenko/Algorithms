f39e0362ddb2913d3c05786cf5f0ac5793fbe767
PetrVakulenko <vakulenkopetya@gmail.com>
2020-01-13 11:15:43 +0200

    Solution for `Minimum time visiting all points`

---
+++ src/Geometry/MinimumTimeVisitingAllPoints_1266/Main.java
@@ -0,0 +1,9 @@
+package Geometry.MinimumTimeVisitingAllPoints_1266;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[][] points = {{1,1},{3,4},{-1,0}};
+        System.out.println(s.minTimeToVisitAllPoints(points));
+    }
+}
