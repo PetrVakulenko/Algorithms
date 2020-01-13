10034ba4508e78e40bde43b6fae51931a1eaa9d2
PetrVakulenko <vakulenkopetya@gmail.com>
2020-01-13 21:01:10 +0200

    Solution for `Find n unique integers sum up to zero`

---
+++ src/Array/FindNUniqueIntegersSumUpToZero_1304/Main.java
@@ -0,0 +1,11 @@
+package Array.FindNUniqueIntegersSumUpToZero_1304;
+
+import Utils.PrintResult;
+
+public class Main {
+    public static void main(String[] argv){
+        Solution s = new Solution();
+
+        PrintResult.printArray(s.sumZero(5));
+    }
+}
