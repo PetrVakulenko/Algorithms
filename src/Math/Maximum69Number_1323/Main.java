6aead01ba3e6ecc376bb6d545b341024ed5b0e9e
PetrVakulenko <vakulenkopetya@gmail.com>
2020-01-19 21:16:25 +0200

    Solution for `Maximum 69 number`

---
+++ src/Math/Maximum69Number_1323/Main.java
@@ -0,0 +1,8 @@
+package Math.Maximum69Number_1323;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.maximum69Number(6699));
+    }
+}
