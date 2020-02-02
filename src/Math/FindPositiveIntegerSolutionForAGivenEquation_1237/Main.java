e25e15c91de5d150dbbede24d85877caa86c7cbe
PetrVakulenko <vakulenkopetya@gmail.com>
2020-02-02 23:06:35 +0200

    Solution for `Find positive integer solution for a given equation`

---
+++ src/Math/FindPositiveIntegerSolutionForAGivenEquation_1237/Main.java
@@ -0,0 +1,9 @@
+package Math.FindPositiveIntegerSolutionForAGivenEquation_1237;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.findSolution(new CustomFunction1(), 5));
+        System.out.println(s.findSolution(new CustomFunction2(), 5));
+    }
+}
