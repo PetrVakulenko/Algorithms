0bbc1b2f971991bee85b64ea31ea4e431dabe0bd
PetrVakulenko <vakulenkopetya@gmail.com>
2020-02-03 21:48:14 +0200

    Solution for `Minimum absolute difference`

---
+++ src/Array/MinimumAbsoluteDifference_1200/Main.java
@@ -0,0 +1,9 @@
+package Array.MinimumAbsoluteDifference_1200;
+
+public class Main {
+    public static void main(String[] argv){
+        Solution s = new Solution();
+        int[] arr = {4,2,1,3};
+        System.out.println(s.minimumAbsDifference(arr));
+    }
+}
