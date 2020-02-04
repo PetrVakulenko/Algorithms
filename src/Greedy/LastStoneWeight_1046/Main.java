87f51601deea30c7a9f8cd53252239b5053be6c9
PetrVakulenko <vakulenkopetya@gmail.com>
2020-02-04 18:18:38 +0200

    Solution for `Last Stone weight`

---
+++ src/Greedy/LastStoneWeight_1046/Main.java
@@ -0,0 +1,9 @@
+package Greedy.LastStoneWeight_1046;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] input = {2, 7, 4, 1, 8, 1};
+        System.out.println(s.lastStoneWeight(input));
+    }
+}
