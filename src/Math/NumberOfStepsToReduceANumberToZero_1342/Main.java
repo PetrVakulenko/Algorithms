9608526b18b289dc29fff0a82762e2494bd79b37
PetrVakulenko <vakulenkopetya@gmail.com>
2020-02-16 18:07:38 +0200

    Solution for `Number of steps to reduce a number to zero`

---
+++ src/Math/NumberOfStepsToReduceANumberToZero_1342/Main.java
@@ -0,0 +1,9 @@
+package Math.NumberOfStepsToReduceANumberToZero_1342;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(s.numberOfSteps(14));
+    }
+}
