d1e8bcfeff871ffd068c0bf7ec9932a3e33f631d
PetrVakulenko <vakulenkopetya@gmail.com>
2020-03-02 14:54:09 +0200

    Solution for `How Many numbers are smaller than the current number`

---
+++ src/HashTable/HowManyNumbersAreSmallerThanTheCurrentNumber_1365/Main.java
@@ -0,0 +1,13 @@
+package HashTable.HowManyNumbersAreSmallerThanTheCurrentNumber_1365;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] nums = {8,1,2,2,3};
+
+        System.out.println(Arrays.toString(s.smallerNumbersThanCurrent(nums)));
+    }
+}
