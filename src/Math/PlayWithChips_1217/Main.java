89fcef568497f75e9a073ff79f167656d2d7ce4b
PetrVakulenko <vakulenkopetya@gmail.com>
2020-03-10 14:38:08 +0200

    Solution for `Play with chips`

---
+++ src/Math/PlayWithChips_1217/Main.java
@@ -0,0 +1,11 @@
+package Math.PlayWithChips_1217;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] chips = {1,2,3};
+
+        System.out.println(s.minCostToMoveChips(chips));
+    }
+}
