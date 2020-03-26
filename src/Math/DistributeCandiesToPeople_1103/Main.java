bd155fb4d9e42bc83fb35de01b230b19ac01842f
PetrVakulenko <vakulenkopetya@gmail.com>
2020-03-26 22:25:28 +0200

    Solution for `Distribute candies to people`

---
+++ src/Math/DistributeCandiesToPeople_1103/Main.java
@@ -0,0 +1,12 @@
+package Math.DistributeCandiesToPeople_1103;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int candies = 7;
+        int num_people = 4;
+        System.out.println(Arrays.toString(s.distributeCandies(candies, num_people)));
+    }
+}
