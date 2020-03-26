5595d5f4b2ef117bd3a2e5ba6e7552866993b1f5
PetrVakulenko <vakulenkopetya@gmail.com>
2020-03-26 22:04:56 +0200

    Solution for `Element appearing more than 25% in sorted array`

--- src/Array/ElementAppearingMoreThan25InSortedArray_1287/Main.java
+++ src/Array/ElementAppearingMoreThan25InSortedArray_1287/Main.java
@@ -3,7 +3,7 @@ package Array.ElementAppearingMoreThan25InSortedArray_1287;
 public class Main {
     public static void main(String[] argv){
         Solution s = new Solution();
-        int[] arr = {1,2,2,6,6,6,6,7,10}
+        int[] arr = {1,2,2,6,6,6,6,7,10};
         System.out.println(s.findSpecialInteger(arr));
     }
 }
