389707f88d3a8e016f7f9b914d7f90cc40f054d6
PetrVakulenko <vakulenkopetya@gmail.com>
2020-03-28 12:40:33 +0200

    Solution for `Find the distance value between two arrays`

---
+++ src/Array/FindTheDistanceValueBetweenTwoArrays_1385/Main.java
@@ -0,0 +1,15 @@
+package Array.FindTheDistanceValueBetweenTwoArrays_1385;
+
+import Utils.PrintResult;
+
+public class Main {
+    public static void main(String[] argv){
+        Solution s = new Solution();
+
+        int[] arr1 = {4,5,8};
+        int[] arr2 = {10,9,1,8};
+        int d = 2;
+
+        System.out.println(s.findTheDistanceValue(arr1, arr2, d));
+    }
+}
