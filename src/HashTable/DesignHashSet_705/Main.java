8f0fba21209d6579df6d9fc63edcc4e65c724698
PetrVakulenko <vakulenkopetya@gmail.com>
2020-03-28 12:59:01 +0200

    Solution for `Design hashset`

---
+++ src/HashTable/DesignHashSet_705/Main.java
@@ -0,0 +1,13 @@
+package HashTable.DesignHashSet_705;
+
+public class Main {
+    public static void main(String[] args) {
+        MyHashSet s = new MyHashSet();
+
+        s.add(1);
+        s.add(2);
+        s.contains(3);
+        s.contains(2);
+        s.remove(2);
+    }
+}
