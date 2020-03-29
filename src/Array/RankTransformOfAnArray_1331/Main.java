a74f125ef24c010473dde24c3c1a099c342c6e8b
PetrVakulenko <vakulenkopetya@gmail.com>
2020-03-29 17:29:41 +0300

    Solution for `Rank transform of an array`

---
+++ src/Array/RankTransformOfAnArray_1331/Main.java
@@ -0,0 +1,11 @@
+package Array.RankTransformOfAnArray_1331;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] arr = {40,10,20,30};
+        System.out.println(Arrays.toString(s.arrayRankTransform(arr)));
+    }
+}
