90e2f175843b4fa9aeb2fdf7b22d7d1a14b03f39
PetrVakulenko <vakulenkopetya@gmail.com>
2020-03-30 23:36:02 +0300

    Solution for `Remove palindromic subsequences`

---
+++ src/String/RemovePalindromicSubsequences_1332/Main.java
@@ -0,0 +1,9 @@
+package String.RemovePalindromicSubsequences_1332;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String str = "ababa";
+        System.out.println(s.removePalindromeSub(str));
+    }
+}
