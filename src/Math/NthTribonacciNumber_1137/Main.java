14f288b90b53880aa1b932aeb5c6034779e4d4da
PetrVakulenko <vakulenkopetya@gmail.com>
2020-03-30 23:58:36 +0300

    Solution for `N-th tribonacci number`

---
+++ src/Math/NthTribonacciNumber_1137/Main.java
@@ -0,0 +1,9 @@
+package Math.NthTribonacciNumber_1137;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(s.tribonacci(4));
+    }
+}
