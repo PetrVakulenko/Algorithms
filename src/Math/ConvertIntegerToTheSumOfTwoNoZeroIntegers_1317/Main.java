a74f74da95c29e2226fb331c1c485f8f21346dbb
PetrVakulenko <vakulenkopetya@gmail.com>
2020-03-30 23:04:43 +0300

    Solution for `Convert integer to the sum of two no zero integers`

---
+++ src/Math/ConvertIntegerToTheSumOfTwoNoZeroIntegers_1317/Main.java
@@ -0,0 +1,11 @@
+package Math.ConvertIntegerToTheSumOfTwoNoZeroIntegers_1317;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int num = 1010;
+        System.out.println(Arrays.toString(s.getNoZeroIntegers(num)));
+    }
+}
