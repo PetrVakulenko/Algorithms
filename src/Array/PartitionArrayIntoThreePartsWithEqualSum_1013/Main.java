8b58b12afb0e233ef2bfd5b7057dd11e4ad79952
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-04 02:11:56 +0300

    Solution for `Partition Array Into Three Parts With Equal Sum`

---
+++ src/Array/PartitionArrayIntoThreePartsWithEqualSum_1013/Main.java
@@ -0,0 +1,9 @@
+package Array.PartitionArrayIntoThreePartsWithEqualSum_1013;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] A = {0,2,1,-6,6,-7,9,1,2,0,1};
+        System.out.println(s.canThreePartsEqualSum(A));
+    }
+}
