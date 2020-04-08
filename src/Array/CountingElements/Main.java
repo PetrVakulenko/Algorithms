7e3adf0d35488499d533e4e0f371b5a2031e45f9
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-08 14:52:00 +0300

    Solution for `Counting elements`

---
+++ src/Array/CountingElements/Main.java
@@ -0,0 +1,11 @@
+package Array.CountingElements;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] arr = {0, 0, 1, 1};
+
+        System.out.println(s.countElements(arr));
+    }
+}
