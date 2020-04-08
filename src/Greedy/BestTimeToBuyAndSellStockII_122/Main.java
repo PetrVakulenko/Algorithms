76feb3f5c1d544ec2146d363a4f6697e3690915e
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-08 15:12:12 +0300

    Solution for `Best time to buy and sell stock 2`

---
+++ src/Greedy/BestTimeToBuyAndSellStockII_122/Main.java
@@ -0,0 +1,9 @@
+package Greedy.BestTimeToBuyAndSellStockII_122;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] arr = {7,1,5,3,6,4};
+        System.out.println(s.maxProfit(arr));
+    }
+}
