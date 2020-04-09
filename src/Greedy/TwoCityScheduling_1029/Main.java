71b7492e9e6171ae2327f9ea1b857341f4d27dbb
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-09 23:41:01 +0300

    Solution for Two city scheduling`

---
+++ src/Greedy/TwoCityScheduling_1029/Main.java
@@ -0,0 +1,9 @@
+package Greedy.TwoCityScheduling_1029;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[][] nums = {{10,20},{30,200},{400,50},{30,20}};
+        System.out.println(s.twoCitySchedCost(nums));
+    }
+}
