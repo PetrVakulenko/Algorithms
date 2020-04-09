532e07ef9888630ab6a4922879b93155b9aeed57
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-09 22:33:20 +0300

    Solution for `Minimum subsequence in non-increasing order`

---
+++ src/Greedy/MinimumSubsequenceInNonIncreasingOrder_1403/Main.java
@@ -0,0 +1,9 @@
+package Greedy.MinimumSubsequenceInNonIncreasingOrder_1403;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {4,3,10,9,8};
+        System.out.println(s.minSubsequence(nums));
+    }
+}
