9f0f7986a9c3797f83a76d7cc019aa2ffdb4c7c9
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-12 14:34:48 +0300

    Solution for `Roman to int`

---
+++ src/Math/RomanToInteger_13/Main.java
@@ -0,0 +1,8 @@
+package Math.RomanToInteger_13;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.romanToInt("DCXXI"));
+    }
+}
