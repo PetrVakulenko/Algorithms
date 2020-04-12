795f33c26e9f6074c16bc566fcde717616c3a746
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-12 22:37:26 +0300

    Solution for `Plus one`

---
+++ src/Array/PlusOne_66/Main.java
@@ -0,0 +1,11 @@
+package Array.PlusOne_66;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] A = {9,9,8,9,9};
+        System.out.println(Arrays.toString(s.plusOne(A)));
+    }
+}
