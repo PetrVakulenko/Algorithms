743c38432e0aa6ee0cc70196a7b63b11d302d543
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-13 00:03:05 +0300

    Solution for `Symmetric tree`

---
+++ src/Tree/SymmetricTree_101/Main.java
@@ -0,0 +1,20 @@
+package Tree.SymmetricTree_101;
+
+import Utils.TreeNode;
+
+public class Main{
+    public static void main(String[] argv) {
+        TreeNode tree1 = new TreeNode(1);
+        TreeNode tree3 = new TreeNode(3);
+        TreeNode tree2 = new TreeNode(2);
+        tree2.left = tree1;
+        tree2.right = tree3;
+        TreeNode tree7 = new TreeNode(7);
+        TreeNode root = new TreeNode(4);
+        root.left = tree2;
+        root.right = tree7;
+
+        Solution s = new Solution();
+        System.out.println(s.isSymmetric(root));
+    }
+}
