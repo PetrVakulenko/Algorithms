87ce0af0cbaf2e1cbb8004ef4de418d7115fa70f
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-12 18:54:23 +0300

    Solution for `Remove element`

---
+++ src/Array/RemoveElement_27/Main.java
@@ -0,0 +1,10 @@
+package Array.RemoveElement_27;
+
+public class Main {
+    public static void main(String[] argv){
+        Solution s = new Solution();
+        int[] elements = {3,2,2,3};
+        int val = 3;
+        System.out.println(s.removeElement(elements, val));
+    }
+}
