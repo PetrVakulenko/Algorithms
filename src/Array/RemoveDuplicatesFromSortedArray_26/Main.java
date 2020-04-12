eaec410ca4404e8aa41af353615c6586eee37f63
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-12 17:50:10 +0300

    Solution for `Remove duplicates from sorted array`

---
+++ src/Array/RemoveDuplicatesFromSortedArray_26/Main.java
@@ -0,0 +1,9 @@
+package Array.RemoveDuplicatesFromSortedArray_26;
+
+public class Main {
+    public static void main(String[] argv){
+        Solution s = new Solution();
+        int[] elements = {1,1,2};
+        System.out.println(s.removeDuplicates(elements));
+    }
+}
