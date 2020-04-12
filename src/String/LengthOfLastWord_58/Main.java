0aa9a08fc8b7323a18e669ed26abeb0375a92a7d
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-12 20:01:15 +0300

    Solution for `Length of last word`

---
+++ src/String/LengthOfLastWord_58/Main.java
@@ -0,0 +1,9 @@
+package String.LengthOfLastWord_58;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String word = "Hello World";
+        System.out.println(s.lengthOfLastWord(word));
+    }
+}
