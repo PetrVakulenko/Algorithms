56aa5ea949b4b0c6f1312168a631c2a8698873b8
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-12 19:58:44 +0300

    Solution for `Two sum for input is a binary search tree`

--- src/Tree/TwoSum4InputIsABST_653/Main.java
+++ src/Tree/TwoSum4InputIsABST_653/Main.java
@@ -4,19 +4,19 @@ import Utils.TreeNode;
 
 public class Main{
     public static void main(String[] argv) {
-        TreeNode tree1 = new TreeNode(1);
-        TreeNode tree3 = new TreeNode(3);
-        TreeNode tree2 = new TreeNode(2);
-        tree2.left = tree1;
-        tree2.right = tree3;
-        TreeNode tree7 = new TreeNode(7);
-        TreeNode root = new TreeNode(4);
-        root.left = tree2;
-        root.right = tree7;
+        TreeNode tree1 = new TreeNode(5);
+        TreeNode tree2 = new TreeNode(3);
+        TreeNode tree3 = new TreeNode(6);
+        tree1.left = tree2;
+        tree1.right = tree3;
+        TreeNode tree4 = new TreeNode(2);
+        TreeNode tree5 = new TreeNode(4);
+        TreeNode tree6 = new TreeNode(7);
+        tree2.left = tree4;
+        tree2.right = tree5;
+        tree3.right = tree6;
 
         Solution s = new Solution();
-        TreeNode resultTree = s.searchBST(root,2);
-        System.out.println("Result:");
-        TreeNode.inorder(resultTree);
+        System.out.println(s.findTarget(tree1, 7));
     }
 }
