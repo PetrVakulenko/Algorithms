3cc339f92cbb884839b30ad473663eca93813bf1
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-12 19:13:50 +0300

    Solution for `Implement strStr()`

---
+++ src/String/ImplementStrStr_28/Main.java
@@ -0,0 +1,12 @@
+package String.ImplementStrStr_28;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        String haystack = "hello";
+        String needle = "ll";
+
+        System.out.println(s.strStr(haystack, needle));
+    }
+}
