d38af78129ea0ed41776eb9ad47e1b9b0466901b
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-12 19:18:05 +0300

    Solution for `Search insert position`

---
+++ src/Array/SearchInsertPosigion_35/Main.java
@@ -0,0 +1,10 @@
+package Array.SearchInsertPosigion_35;
+
+public class Main {
+    public static void main(String[] argv){
+        Solution s = new Solution();
+        int[] input = {1,3,5,6};
+        int target = 2;
+        System.out.println(s.searchInsert(input, target));
+    }
+}
