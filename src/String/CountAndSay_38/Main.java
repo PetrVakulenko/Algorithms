de09315f11cc5380d9f9b22271c9fb494b6d53a0
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-12 20:56:53 +0300

    Solution for `Count and say`

---
+++ src/String/CountAndSay_38/Main.java
@@ -0,0 +1,9 @@
+package String.CountAndSay_38;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(s.countAndSay(5));
+    }
+}
