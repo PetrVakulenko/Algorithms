d4e651f17f512c8e8f0dd45643a64643d51da4c2
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-16 23:43:48 +0300

    Solution for `Factorial trailing zeroes`

---
+++ src/Math/FactorialTrailingZeroes_172/Main.java
@@ -0,0 +1,8 @@
+package Math.FactorialTrailingZeroes_172;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.trailingZeroes(6));
+    }
+}
