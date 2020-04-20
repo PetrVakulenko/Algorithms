f9d67449658a956e5d002aaa556f3555604a51e9
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-20 11:08:13 +0300

    Solution for `Search in rotated sorted array`

---
+++ src/BinarySearch/SearchInRotatedSortedArray_33/Main.java
@@ -0,0 +1,9 @@
+package BinarySearch.SearchInRotatedSortedArray_33;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] arr = {4,5,6,7,0,1,2};
+        System.out.println(s.search(arr, 0));
+    }
+}
