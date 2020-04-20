c8438e0c4e6c57f2b504fb1427aa7cb90e9b262a
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-20 22:10:11 +0300

    Solution for `Contains duplicate`

---
+++ src/HashTable/ContainsDuplicate_217/Main.java
@@ -0,0 +1,9 @@
+package HashTable.ContainsDuplicate_217;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] input = {0,0,1,0,0,0,1,1};
+        System.out.println(s.containsDuplicate(input));
+    }
+}
