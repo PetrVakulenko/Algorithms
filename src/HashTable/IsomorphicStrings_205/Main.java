3f5964b8d48ac874acfba6a9f55b69ec69afe1a9
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-20 21:58:55 +0300

    Solution for `Isomorphic strings`

---
+++ src/HashTable/IsomorphicStrings_205/Main.java
@@ -0,0 +1,9 @@
+package HashTable.IsomorphicStrings_205;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(s.isIsomorphic("egg", "odd"));
+    }
+}
