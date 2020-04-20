6ec82ca635384874d3cee068bd3bb597cc9c0855
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-21 01:05:59 +0300

    Solution for `Sum of two integers`

---
+++ src/BitManipulation/SumOfTwoIntegers_371/Main.java
@@ -0,0 +1,9 @@
+package BitManipulation.SumOfTwoIntegers_371;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(s.getSum(5,3));
+    }
+}
