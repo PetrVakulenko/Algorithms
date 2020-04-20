2f5e487bc0c0020ca701f8862f1e3c26e4df41a0
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-20 12:16:16 +0300

    Solution for `Number of 1 bits`

---
+++ src/BitManipulation/NumberOf1Bits_191/Main.java
@@ -0,0 +1,9 @@
+package BitManipulation.NumberOf1Bits_191;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(s.hammingWeight(32));
+    }
+}
