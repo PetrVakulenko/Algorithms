f528145bae5b474516b3e8182dd85b2e393a817e
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-20 22:57:43 +0300

    Solution for `Power of two`

---
+++ src/Math/PowerOfTwo_231/Main.java
@@ -0,0 +1,9 @@
+package Math.PowerOfTwo_231;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(s.isPowerOfTwo(256));
+    }
+}
