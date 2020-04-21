dac13c3ab52c37b94c854840adcc101b735bcf67
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-21 19:40:55 +0300

    Solution for `Can place flowers`

---
+++ src/Array/CanPlaceFlowers_605/Main.java
@@ -0,0 +1,11 @@
+package Array.CanPlaceFlowers_605;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] flowerbed = {1,0,0,0,1};
+        int n = 1;
+
+        System.out.println(s.canPlaceFlowers(flowerbed, n));
+    }
+}
