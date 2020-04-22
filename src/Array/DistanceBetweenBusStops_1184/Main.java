72354372811398f40a5a9f4968a8e472534c1df7
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-23 00:20:22 +0300

    Solution for `Distance between bus stops`

---
+++ src/Array/DistanceBetweenBusStops_1184/Main.java
@@ -0,0 +1,9 @@
+package Array.DistanceBetweenBusStops_1184;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {1,2,3,4};
+        System.out.println(s.distanceBetweenBusStops(nums, 1,2));
+    }
+}
