f4dbd8a5cabb5dbf3019fbdfa739336ea31a9c0a
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-22 22:35:20 +0300

    Solution for `Minimum value to get positive step by step sum`

---
+++ src/Array/MinimumValueToGetPositiveStepByStepSum_1413/Main.java
@@ -0,0 +1,9 @@
+package Array.MinimumValueToGetPositiveStepByStepSum_1413;
+
+public class Main {
+    public static void main(String[] argv){
+        Solution s = new Solution();
+        int[] arr = {1,-2,-3};
+        System.out.println(s.minStartValue(arr));
+    }
+}
