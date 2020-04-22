3cbdfbefc7c8b1c0687159b94f557d3efb531724
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-22 22:06:57 +0300

    Solution for `Subarray sum equals k`

---
+++ src/Array/SubarraySumEqualsK_560/Main.java
@@ -0,0 +1,9 @@
+package Array.SubarraySumEqualsK_560;
+
+public class Main {
+    public static void main(String[] argv){
+        Solution s = new Solution();
+        int[] array = {1,1,1};
+        System.out.println(s.subarraySum(array, 2));
+    }
+}
