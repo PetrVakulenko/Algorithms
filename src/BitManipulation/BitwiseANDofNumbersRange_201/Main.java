d5be4bbcc08f42f8d57f14d42894932c30916f3a
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-23 21:47:12 +0300

    Solution for `Bitwise and of numbers range`

---
+++ src/BitManipulation/BitwiseANDofNumbersRange_201/Main.java
@@ -0,0 +1,9 @@
+package BitManipulation.BitwiseANDofNumbersRange_201;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(s.rangeBitwiseAnd(2, 4));
+    }
+}
