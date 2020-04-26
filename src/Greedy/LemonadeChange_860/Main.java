380477970626bcc377576a3548d0fab4aac9d614
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-27 00:27:17 +0300

    Solution for `Lemonade change`

---
+++ src/Greedy/LemonadeChange_860/Main.java
@@ -0,0 +1,9 @@
+package Greedy.LemonadeChange_860;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] input = {5,5,5,10,20};
+        System.out.println(s.lemonadeChange(input));
+    }
+}
