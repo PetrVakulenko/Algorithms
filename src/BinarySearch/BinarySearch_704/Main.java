d8d30d9d803153944d007bd7c98610cabb359148
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-27 00:38:09 +0300

    Solution for `Binary search`

---
+++ src/BinarySearch/BinarySearch_704/Main.java
@@ -0,0 +1,11 @@
+package BinarySearch.BinarySearch_704;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] nums = {-1,0,3,5,9,12};
+
+        System.out.println(s.search(nums, 9));
+    }
+}
