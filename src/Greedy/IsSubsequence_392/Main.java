2eb657d979f7d020a7cde6db840dcde7edc52857
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-28 01:43:27 +0300

    Solution for `Is subsequence`

---
+++ src/Greedy/IsSubsequence_392/Main.java
@@ -0,0 +1,8 @@
+package Greedy.IsSubsequence_392;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.isSubsequence("abc", "ahbgdc"));
+    }
+}
