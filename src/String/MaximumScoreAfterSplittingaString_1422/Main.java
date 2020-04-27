694e92db8ca57847eb5482f30aadb372db5edebd
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-27 10:46:05 +0300

    Solution for `Maximum score after splitting a string`

---
+++ src/String/MaximumScoreAfterSplittingaString_1422/Main.java
@@ -0,0 +1,8 @@
+package String.MaximumScoreAfterSplittingaString_1422;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.maxScore("011101"));
+    }
+}
