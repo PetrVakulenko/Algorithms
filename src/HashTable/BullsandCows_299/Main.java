4e6b29be1606dfb73f50fb2143842d55bc61f5be
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-29 21:02:56 +0300

    Solution for `Bulls and cows`

---
+++ src/HashTable/BullsandCows_299/Main.java
@@ -0,0 +1,9 @@
+package HashTable.BullsandCows_299;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(s.getHint("1807", "7810"));
+    }
+}
