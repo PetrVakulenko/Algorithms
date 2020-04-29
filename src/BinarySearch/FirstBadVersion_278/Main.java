b454af1872517fbaaf1aaf4f63de1e23cf85bbe6
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-29 19:46:07 +0300

    Solution for `First bad version`

---
+++ src/BinarySearch/FirstBadVersion_278/Main.java
@@ -0,0 +1,9 @@
+package BinarySearch.FirstBadVersion_278;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution(4);
+
+        System.out.println(s.firstBadVersion(5));
+    }
+}
