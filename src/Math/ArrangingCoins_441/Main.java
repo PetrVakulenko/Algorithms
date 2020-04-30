51c69ee2783a00ee28d7def53bc91b0b6f88b9f2
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-30 21:52:37 +0300

    Solution for `Arranging coins`

---
+++ src/Math/ArrangingCoins_441/Main.java
@@ -0,0 +1,9 @@
+package Math.ArrangingCoins_441;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int digit= 123;
+        System.out.println(s.arrangeCoins(digit));
+    }
+}
