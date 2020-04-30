564256089374e4a56da89fda4e2877e7f9698500
PetrVakulenko <vakulenkopetya@gmail.com>
2020-04-30 22:21:10 +0300

    Solution for `Assign cookies`

---
+++ src/Greedy/AssignCookies_455/Main.java
@@ -0,0 +1,11 @@
+package Greedy.AssignCookies_455;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] g = {1,2,3};
+        int[] z = {1,4,1,1,1};
+
+        System.out.println(s.findContentChildren(g, z));
+    }
+}
