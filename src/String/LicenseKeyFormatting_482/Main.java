3d8db79533c535f756ea38d6fce2cedb59248cf8
PetrVakulenko <vakulenkopetya@gmail.com>
2020-05-03 10:51:50 +0300

    Solution for `License key formatting`

---
+++ src/String/LicenseKeyFormatting_482/Main.java
@@ -0,0 +1,9 @@
+package String.LicenseKeyFormatting_482;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String word = "5F3Z-2e-9-w";
+        System.out.println(s.licenseKeyFormatting(word, 4));
+    }
+}
