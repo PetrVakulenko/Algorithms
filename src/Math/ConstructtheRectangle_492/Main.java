797d6c9a409fd5e4fa059a134bab32b8fb6100a2
PetrVakulenko <vakulenkopetya@gmail.com>
2020-05-03 12:09:58 +0300

    Solution for `Construct the rectangle`

---
+++ src/Math/ConstructtheRectangle_492/Main.java
@@ -0,0 +1,11 @@
+package Math.ConstructtheRectangle_492;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int num = 6;
+        System.out.println(Arrays.toString(s.constructRectangle(num)));
+    }
+}
