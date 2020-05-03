ebc5e4ff7ee7c85d7aad5c2ea6e4902e6db9eb38
PetrVakulenko <vakulenkopetya@gmail.com>
2020-05-03 12:02:39 +0300

    Solution for `String rompression`

---
+++ src/String/StringCompression_443/Main.java
@@ -0,0 +1,9 @@
+package String.StringCompression_443;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        char arr[] = {'a','a','b','b','c','c','c'};
+        System.out.println(s.compress(arr));
+    }
+}
