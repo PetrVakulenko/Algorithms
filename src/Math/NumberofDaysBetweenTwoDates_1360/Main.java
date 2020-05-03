579bb4e9a056941c2a25e25dbaf4b42954ba4eef
PetrVakulenko <vakulenkopetya@gmail.com>
2020-05-03 12:05:50 +0300

    Solution for `Number of days between two dates`

---
+++ src/Math/NumberofDaysBetweenTwoDates_1360/Main.java
@@ -0,0 +1,9 @@
+package Math.NumberofDaysBetweenTwoDates_1360;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(s.daysBetweenDates("2019-06-29", "2019-06-30"));
+    }
+}
