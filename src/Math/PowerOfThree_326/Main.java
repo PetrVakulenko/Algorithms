5f73822d7ac2b7c0719d1f07cfc67a2745b9fdf8
PetrVakulenko <vakulenkopetya@gmail.com>
2020-05-03 10:36:48 +0300

    Solution for `Power of three`

---
+++ src/Math/PowerOfThree_326/Main.java
@@ -0,0 +1,9 @@
+package Math.PowerOfThree_326;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(s.isPowerOfThree(27));
+    }
+}
