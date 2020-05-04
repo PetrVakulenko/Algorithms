ad2819445504a0941641ff90c2bb55046c42f426
PetrVakulenko <vakulenkopetya@gmail.com>
2020-05-04 12:10:32 +0300

    Solution for `Valid mountain array`

---
+++ src/Array/ValidMountainArray_941/Main.java
@@ -0,0 +1,9 @@
+package Array.ValidMountainArray_941;
+
+public class Main {
+    public static void main(String[] argv){
+        Solution s = new Solution();
+        int[] input = {0,3,2,1};
+        System.out.println(s.validMountainArray(input));
+    }
+}
