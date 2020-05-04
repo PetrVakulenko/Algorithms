e3537d3b3ac934ebb24e7359b66fbbcffe297760
PetrVakulenko <vakulenkopetya@gmail.com>
2020-05-04 12:32:23 +0300

    Solution for `Heaters`

---
+++ src/BinarySearch/Heaters_475/Main.java
@@ -0,0 +1,12 @@
+package BinarySearch.Heaters_475;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] input = {1,2,3,4};
+        int[] heaters = {1,2};
+
+        System.out.println(s.findRadius(input, heaters));
+    }
+}
