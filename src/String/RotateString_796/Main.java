b9866f36f3ccde2b9ffaac98f375a0be613eb150
PetrVakulenko <vakulenkopetya@gmail.com>
2020-05-06 23:44:54 +0300

    Solution for `Rotate string`

---
+++ src/String/RotateString_796/Main.java
@@ -0,0 +1,8 @@
+package String.RotateString_796;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.rotateString("abcde", "cdeab"));
+    }
+}
