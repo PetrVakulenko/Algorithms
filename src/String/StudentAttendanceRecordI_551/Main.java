040fb52e998f13f67c697da3949b07b12a2eb287
PetrVakulenko <vakulenkopetya@gmail.com>
2020-05-06 23:41:57 +0300

    Solution for `Student attendance record 1`

---
+++ src/String/StudentAttendanceRecordI_551/Main.java
@@ -0,0 +1,8 @@
+package String.StudentAttendanceRecordI_551;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.checkRecord("PPALLP"));
+    }
+}
