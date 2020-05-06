82ca55cb389b8495416bf92030e0065329b6f00d
PetrVakulenko <vakulenkopetya@gmail.com>
2020-05-06 23:51:03 +0300

    Solution for `Largest number at least twice of others`

---
+++ src/Array/LargestNumberAtLeastTwiceOfOthers_747/Main.java
@@ -0,0 +1,9 @@
+package Array.LargestNumberAtLeastTwiceOfOthers_747;
+
+public class Main {
+    public static void main(String[] argv){
+        Solution s = new Solution();
+        int[] example = {3,6,1,0};
+        System.out.println(s.dominantIndex(example));
+    }
+}
