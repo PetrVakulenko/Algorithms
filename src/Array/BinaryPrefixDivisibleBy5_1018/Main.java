70ed39655b1711cd7b8ba5ed23733ed228cbc1ac
PetrVakulenko <vakulenkopetya@gmail.com>
2020-05-11 23:05:08 +0300

    Solution for `Binary prefix divisible by 5`

---
+++ src/Array/BinaryPrefixDivisibleBy5_1018/Main.java
@@ -0,0 +1,9 @@
+package Array.BinaryPrefixDivisibleBy5_1018;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] input = {0,1,1};
+        System.out.println(s.prefixesDivBy5(input));
+    }
+}
