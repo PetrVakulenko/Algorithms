27a07a530d25c4dcc9b6652c5e390de953b65fcf
PetrVakulenko <vakulenkopetya@gmail.com>
2020-05-11 23:03:11 +0300

    Solution for `Build an array with stack operations`

---
+++ src/Stack/BuildanArrayWithStackOperations_1441/Main.java
@@ -0,0 +1,11 @@
+package Stack.BuildanArrayWithStackOperations_1441;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] input = {1,3};
+
+        System.out.println(s.buildArray(input, 3));
+    }
+}
