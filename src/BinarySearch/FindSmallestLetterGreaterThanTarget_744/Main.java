b92d31f85f90354afb38b87d4774821904b3e626
PetrVakulenko <vakulenkopetya@gmail.com>
2020-05-11 22:48:43 +0300

    Solution for `Find smallest letter greater than target`

---
+++ src/BinarySearch/FindSmallestLetterGreaterThanTarget_744/Main.java
@@ -0,0 +1,11 @@
+package BinarySearch.FindSmallestLetterGreaterThanTarget_744;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        char[] letters = {'c', 'f', 'j'};
+
+        System.out.println(s.nextGreatestLetter(letters, 'c'));
+    }
+}
