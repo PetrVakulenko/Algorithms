edd58597d097e1b2d8da83ad6d0cb72a80c1565d
PetrVakulenko <vakulenkopetya@gmail.com>
2020-05-12 10:49:58 +0300

    Solution for `Check if n and its double exist`

---
+++ src/Array/CheckIfNandItsDoubleExist_1346/Main.java
@@ -0,0 +1,11 @@
+package Array.CheckIfNandItsDoubleExist_1346;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] input = {7,1,14,11};
+
+        System.out.println(s.checkIfExist(input));
+    }
+}
