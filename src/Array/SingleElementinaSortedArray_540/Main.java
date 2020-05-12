b6b70b69ffa5e565fcf24c05fe36faa55089157d
PetrVakulenko <vakulenkopetya@gmail.com>
2020-05-12 10:43:50 +0300

    Solution for `Single element in a sorted array`

---
+++ src/Array/SingleElementinaSortedArray_540/Main.java
@@ -0,0 +1,10 @@
+package Array.SingleElementinaSortedArray_540;
+
+public class Main {
+    public static void main(String[] argv){
+        Solution s = new Solution();
+        int[] input = {1,1,2,3,3,4,4,8,8};
+
+        System.out.println(s.singleNonDuplicate(input));
+    }
+}
