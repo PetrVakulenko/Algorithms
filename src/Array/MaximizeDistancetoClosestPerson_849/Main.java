4a1ae6a606e6d6dbd334d334ad489cf10b5abab3
PetrVakulenko <vakulenkopetya@gmail.com>
2020-05-12 10:39:35 +0300

    Solution for `Maximize distance to closest person`

---
+++ src/Array/MaximizeDistancetoClosestPerson_849/Main.java
@@ -0,0 +1,9 @@
+package Array.MaximizeDistancetoClosestPerson_849;
+
+public class Main {
+    public static void main(String[] argv){
+        Solution s = new Solution();
+        int[] arr = {1, 0, 1, 1, 0, 1};
+        System.out.println(s.maxDistToClosest(arr));
+    }
+}
