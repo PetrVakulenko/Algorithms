06c87aa6cf30f1687c3fb8c173b897ece6f0f725
PetrVakulenko <vakulenkopetya@gmail.com>
2020-05-18 15:00:54 +0300

    Solution for `Find pivot index`

---
+++ src/Array/FindPivotIndex_724/Main.java
@@ -0,0 +1,11 @@
+package Array.FindPivotIndex_724;
+
+public class Main {
+    public static void main(String[] argv){
+        Solution s = new Solution();
+
+        int[] nums = {1, 7, 3, 6, 5, 6};
+
+        System.out.println(s.pivotIndex(nums));
+    }
+}
