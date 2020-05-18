a4aed41cc07639db30317da3d09ec98fc19bd874
PetrVakulenko <vakulenkopetya@gmail.com>
2020-05-18 15:50:51 +0300

    Solution for `Moving stones until consequtive`

---
+++ src/Others/MovingStonesUntilConsecutive_1033/Main.java
@@ -0,0 +1,10 @@
+package Others.MovingStonesUntilConsecutive_1033;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(Arrays.toString(s.numMovesStones(3,5,1));
+    }
+}
