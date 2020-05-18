951b5d78416d8c7583d428bde9e6fd8c55f305a4
PetrVakulenko <vakulenkopetya@gmail.com>
2020-05-18 15:02:47 +0300

    Solution for `Add to array form of integer`

---
+++ src/Array/AddtoArrayFormofInteger_989/Main.java
@@ -0,0 +1,10 @@
+package Array.AddtoArrayFormofInteger_989;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int arr[] = {1,4,3,2};
+        int K = 806;
+        System.out.println(s.addToArrayForm(arr, K));
+    }
+}
