ea49d162a7a021985d3b46275450660088c589f4
PetrVakulenko <vakulenkopetya@gmail.com>
2020-05-18 15:11:19 +0300

    Solution for `Consecutive characters`

---
+++ src/String/ConsecutiveCharacters_1446/Solution.java
@@ -0,0 +1,53 @@
+package String.ConsecutiveCharacters_1446;
+
+/**
+ 1446. Consecutive Characters
+
+ Given a string s, the power of the string is the maximum length of a non-empty substring that contains only one unique character.
+ Return the power of the string.
+
+ Example 1:
+ Input: s = "leetcode"
+ Output: 2
+ Explanation: The substring "ee" is of length 2 with the character 'e' only.
+
+ Example 2:
+ Input: s = "abbcccddddeeeeedcba"
+ Output: 5
+ Explanation: The substring "eeeee" is of length 5 with the character 'e' only.
+
+ Example 3:
+ Input: s = "triplepillooooow"
+ Output: 5
+
+ Example 4:
+ Input: s = "hooraaaaaaaaaaay"
+ Output: 11
+
+ Example 5:
+ Input: s = "tourist"
+ Output: 1
+
+ Constraints:
+ 1 <= s.length <= 500
+ s contains only lowercase English letters.
+ */
+
+public class Solution {
+    public int maxPower(String s) {
+        int power = 1;
+
+        int maxPower = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i-1)) {
+                power++;
+            } else {
+                maxPower = Math.max(power, maxPower);
+                power = 1;
+            }
+        }
+
+        return Math.max(power, maxPower);
+    }
+}
