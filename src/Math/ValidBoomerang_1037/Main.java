d588eaebccb48c7f0662304d6c172935aa87536b
PetrVakulenko <vakulenkopetya@gmail.com>
2020-05-18 15:14:20 +0300

    Solution for `Valid boomerang`

---
+++ src/Math/ValidBoomerang_1037/Main.java
@@ -0,0 +1,11 @@
+package Math.ValidBoomerang_1037;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[][] points = {{1,1}, {2,3}, {3,2}};
+
+        System.out.println(s.isBoomerang(points));
+    }
+}
