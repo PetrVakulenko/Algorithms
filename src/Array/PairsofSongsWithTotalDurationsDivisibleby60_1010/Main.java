c21e4d6b30d08d1a6fc82b24c2719b3748fb4a6e
PetrVakulenko <vakulenkopetya@gmail.com>
2020-05-18 14:05:37 +0300

    Solution for `Pairs of songs with total durations divisable by 60`

---
+++ src/Array/PairsofSongsWithTotalDurationsDivisibleby60_1010/Main.java
@@ -0,0 +1,11 @@
+package Array.PairsofSongsWithTotalDurationsDivisibleby60_1010;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] input = {30,20,150,100,40};
+
+        System.out.println(s.numPairsDivisibleBy60(input));
+    }
+}
