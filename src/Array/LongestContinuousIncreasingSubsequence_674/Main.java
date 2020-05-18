b5ab328a31e95bb436dde213d1984fe12217452f
PetrVakulenko <vakulenkopetya@gmail.com>
2020-05-18 14:01:14 +0300

    Solution for `Longest continuous increasing subsequence`

---
+++ src/Array/LongestContinuousIncreasingSubsequence_674/Main.java
@@ -0,0 +1,9 @@
+package Array.LongestContinuousIncreasingSubsequence_674;
+
+public class Main {
+    public static void main(String[] argv){
+        Solution s = new Solution();
+        int[] example = {1,3,5,4,7};
+        System.out.println(s.findLengthOfLCIS(example));
+    }
+}
