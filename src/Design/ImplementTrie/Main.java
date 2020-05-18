8b07c4e988079ea5d2335311e0c589fde725a01b
PetrVakulenko <vakulenkopetya@gmail.com>
2020-05-18 14:09:02 +0300

    Solution for `Implement trie`

---
+++ src/Design/ImplementTrie/Main.java
@@ -0,0 +1,14 @@
+package Design.ImplementTrie;
+
+public class Main {
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+
+        trie.insert("apple");
+        trie.search("apple");   // returns true
+        trie.search("app");     // returns false
+        trie.startsWith("app"); // returns true
+        trie.insert("app");
+        trie.search("app");     // returns true
+    }
+}
