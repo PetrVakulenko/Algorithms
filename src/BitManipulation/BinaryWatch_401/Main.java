dadc795fc15d8a843d94de7a3678c57f53638e41
PetrVakulenko <vakulenkopetya@gmail.com>
2020-05-27 12:33:18 +0300

    Solution for `Bit manipulation`

---
+++ src/BitManipulation/BinaryWatch_401/Main.java
@@ -0,0 +1,9 @@
+package BitManipulation.BinaryWatch_401;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(s.readBinaryWatch(1));
+    }
+}
