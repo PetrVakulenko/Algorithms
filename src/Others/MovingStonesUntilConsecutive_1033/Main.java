646679a61037168f7a5bc4bc44de5f752efffc03
PetrVakulenko <vakulenkopetya@gmail.com>
2020-05-27 12:12:34 +0300

    Fixed code style

--- src/Others/MovingStonesUntilConsecutive_1033/Main.java
+++ src/Others/MovingStonesUntilConsecutive_1033/Main.java
@@ -5,6 +5,6 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(Arrays.toString(s.numMovesStones(3,5,1));
+        System.out.println(Arrays.toString(s.numMovesStones(3,5,1)));
     }
 }
