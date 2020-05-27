c2b73b1b4d93c1b0a26633d3d789056380a76a86
PetrVakulenko <vakulenkopetya@gmail.com>
2020-05-27 12:16:34 +0300

    Solution for `1-bit and 2-bit characyers`

---
+++ src/Array/OneBitAndTwoBitCharacters_717/Main.java
@@ -0,0 +1,11 @@
+package Array.OneBitAndTwoBitCharacters_717;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] bits = {1,0,0};
+
+        System.out.println(s.isOneBitCharacter(bits));
+    }
+}
