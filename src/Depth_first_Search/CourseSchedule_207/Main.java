1dbd1b98bebb7fa10ab80218e13babe1900accf5
PetrVakulenko <vakulenkopetya@gmail.com>
2020-06-07 20:57:50 +0300

    Solution for `Cource schedule`

---
+++ src/Depth_first_Search/CourseSchedule_207/Main.java
@@ -0,0 +1,11 @@
+package Depth_first_Search.CourseSchedule_207;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[][] prerequisites = {{1,0}};
+
+        System.out.println(s.canFinish(2, prerequisites));
+    }
+}
