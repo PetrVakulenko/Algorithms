3ef3370366f591d200c4f71a0cff0f00937fc6c8
PetrVakulenko <vakulenkopetya@gmail.com>
2020-06-07 20:41:47 +0300

    Solution for `Uncrossed lines`

---
+++ src/Array/UncrossedLines_1035/Main.java
@@ -0,0 +1,9 @@
+package Array.UncrossedLines_1035;
+
+public class Main {
+    public static void main(String[] argv){
+        Solution s = new Solution();
+        int[] A = {1,4,2}, B = {1,2,4};
+        System.out.println(s.maxUncrossedLines(A, B));
+    }
+}
