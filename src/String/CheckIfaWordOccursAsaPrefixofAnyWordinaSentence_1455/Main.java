fce7dca12e4ddf42489e203de47a86815b82a3a1
PetrVakulenko <vakulenkopetya@gmail.com>
2020-06-07 21:10:57 +0300

    Solution for `Check if a word occurs a prefix of any word in a sentence`

---
+++ src/String/CheckIfaWordOccursAsaPrefixofAnyWordinaSentence_1455/Main.java
@@ -0,0 +1,12 @@
+package String.CheckIfaWordOccursAsaPrefixofAnyWordinaSentence_1455;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        String S = "i use triple pillow";
+        String T = "pill";
+
+        System.out.println(s.isPrefixOfWord(S, T));
+    }
+}
