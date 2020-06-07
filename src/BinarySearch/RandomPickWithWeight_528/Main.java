8375181c231bec365466a62c94c4491d4d708499
PetrVakulenko <vakulenkopetya@gmail.com>
2020-06-07 21:15:54 +0300

    Solution for `Random pick with height`

---
+++ src/BinarySearch/RandomPickWithWeight_528/Main.java
@@ -0,0 +1,13 @@
+package BinarySearch.RandomPickWithWeight_528;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] input = {1, 3};
+
+        Solution s = new Solution(input);
+
+        System.out.println(s.pickIndex());
+        System.out.println(s.pickIndex());
+        System.out.println(s.pickIndex());
+    }
+}
