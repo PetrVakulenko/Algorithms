77e9bdeea006dfa589bf4aa78fbcdd5d590433a0
PetrVakulenko <vakulenkopetya@gmail.com>
2020-06-07 20:44:10 +0300

    Solution for `Shuffle the array`

---
+++ src/Array/ShuffletheArray_1470/Main.java
@@ -0,0 +1,12 @@
+package Array.ShuffletheArray_1470;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] argv){
+        Solution s = new Solution();
+        int[] input = {2, 5, 1, 3, 4, 7};
+
+        System.out.println(Arrays.toString(s.shuffle(input, 3)));
+    }
+}
