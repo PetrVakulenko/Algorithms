8004930483e5681401c6bee1c15749847b97d236
PetrVakulenko <vakulenkopetya@gmail.com>
2020-06-07 21:05:54 +0300

    Solution for `Maximum product of two elements in an array`

---
+++ src/Array/MaximumProductofTwoElementsinanArray_1464/Main.java
@@ -0,0 +1,9 @@
+package Array.MaximumProductofTwoElementsinanArray_1464;
+
+public class Main {
+    public static void main(String[] argv){
+        Solution s = new Solution();
+        int[] arr = {1,5,4,5};
+        System.out.println(s.maxProduct(arr));
+    }
+}
