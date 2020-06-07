0fbae1db673419dce31483f0e17f95728218793d
PetrVakulenko <vakulenkopetya@gmail.com>
2020-06-07 21:08:08 +0300

    Solution for `Make two arrays equal byt reversing subarrays`

---
+++ src/Array/MakeTwoArraysEqualbyReversingSubarrays_1460/Main.java
@@ -0,0 +1,9 @@
+package Array.MakeTwoArraysEqualbyReversingSubarrays_1460;
+
+public class Main {
+    public static void main(String[] argv){
+        Solution s = new Solution();
+        int[] target = {1,12}, arr = {12,1};
+        System.out.println(s.canBeEqual(target, arr));
+    }
+}
