d58d908413c9a8a08e243b3624bab81ccb6578d7
PetrVakulenko <vakulenkopetya@gmail.com>
2020-06-07 20:48:57 +0300

    Solution for `Coin change2`

---
+++ src/Array/CoinChange2_518/Main.java
@@ -0,0 +1,11 @@
+package Array.CoinChange2_518;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] coins = {1, 2, 5};
+
+        System.out.println(s.change(5, coins));
+    }
+}
