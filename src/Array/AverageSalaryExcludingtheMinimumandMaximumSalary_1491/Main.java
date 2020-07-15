efb9a6e583840f124b9d554b689d025559135829
PetrVakulenko <vakulenkopetya@gmail.com>
2020-07-15 11:05:23 +0300

    Solution for `Average salary excluding the maximum and minimum salary`

---
+++ src/Array/AverageSalaryExcludingtheMinimumandMaximumSalary_1491/Main.java
@@ -0,0 +1,11 @@
+package Array.AverageSalaryExcludingtheMinimumandMaximumSalary_1491;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] salaries = {1000,2000,3000};
+
+        System.out.println(s.average(salaries));
+    }
+}
