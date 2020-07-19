550b1207508112a9b80e063598ccf2e539aa8594
PetrVakulenko <vakulenkopetya@gmail.com>
2020-07-19 22:31:48 +0300

    Solution for `Can Make Arithmetic Progression From Sequence`

---
+++ src/Array/CanMakeArithmeticProgressionFromSequence_1502/Main.java
@@ -0,0 +1,9 @@
+package Array.CanMakeArithmeticProgressionFromSequence_1502;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] input = {3,5,1};
+        System.out.println(s.canMakeArithmeticProgression(input));
+    }
+}
