5d5d83275f216381e8df77735a2d5d260b13bdda
PetrVakulenko <vakulenkopetya@gmail.com>
2020-07-19 22:37:22 +0300

    Solution for `Running sum of 1d array`

---
+++ src/Array/RunningSumof1dArray_1480/Main.java
@@ -0,0 +1,11 @@
+package Array.RunningSumof1dArray_1480;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] argv){
+        Solution s = new Solution();
+        int[] input = {1,2,3,4};
+        System.out.println(Arrays.toString(s.runningSum(input)));
+    }
+}
