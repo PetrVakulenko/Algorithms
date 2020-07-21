d38e5a799ba1fb1ca78ecd1e7cf407a428752457
PetrVakulenko <vakulenkopetya@gmail.com>
2020-07-21 12:30:55 +0300

    Solution for `Xor operation in an array`

---
+++ src/BitManipulation/XOROperationinanArray_1486/Main.java
@@ -0,0 +1,9 @@
+package BitManipulation.XOROperationinanArray_1486;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(s.xorOperation(5,0));
+    }
+}
