5108b97682b24bc792ed9415e6c12b39b79763da
PetrVakulenko <vakulenkopetya@gmail.com>
2020-07-21 11:08:59 +0300

    Solution for `Final prices with a special discount in a shop`

---
+++ src/Array/FinalPricesWithaSpecialDiscountinaShop_1475/Main.java
@@ -0,0 +1,11 @@
+package Array.FinalPricesWithaSpecialDiscountinaShop_1475;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] argv){
+        Solution s = new Solution();
+        int input[] = {4,2,4,2,3};
+        System.out.println(Arrays.toString(s.finalPrices(input)));
+    }
+}
