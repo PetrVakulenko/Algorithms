baba402d0e74fffe1d671ec301615a7aea1c912a
PetrVakulenko <vakulenkopetya@gmail.com>
2020-07-22 18:54:16 +0300

    Solution for `3Sum`

---
+++ src/TwoPointers/Sum3_15/Main.java
@@ -0,0 +1,11 @@
+package TwoPointers.Sum3_15;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] input = {-1, 0, 1, 2, -1, -4};
+
+        System.out.println(s.threeSum(input));
+    }
+}
