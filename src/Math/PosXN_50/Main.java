9dc3ddbb4b687c92fbeedfbe6421bfc30fcbea27
PetrVakulenko <vakulenkopetya@gmail.com>
2020-07-22 19:01:59 +0300

    Solution for `Pow x to n`

---
+++ src/Math/PosXN_50/Main.java
@@ -0,0 +1,9 @@
+package Math.PosXN_50;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(s.myPow(2,3));
+    }
+}
