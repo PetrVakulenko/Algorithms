22ebe249f02990f00ed141589b1421dfa8234fdf
PetrVakulenko <vakulenkopetya@gmail.com>
2020-07-23 10:59:07 +0300

    Solution for `Reformat date`

---
+++ src/String/ReformatDate_1507/Main.java
@@ -0,0 +1,8 @@
+package String.ReformatDate_1507;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.reformatDate("2052-10-20"));
+    }
+}
