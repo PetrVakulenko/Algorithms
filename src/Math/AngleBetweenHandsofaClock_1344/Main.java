930ca9386534b25ce4ace56804f3ede2e4545124
PetrVakulenko <vakulenkopetya@gmail.com>
2020-07-23 10:50:34 +0300

    Solution for `Angle between hands of a clock`

---
+++ src/Math/AngleBetweenHandsofaClock_1344/Main.java
@@ -0,0 +1,8 @@
+package Math.AngleBetweenHandsofaClock_1344;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.angleClock(12, 30));
+    }
+}
