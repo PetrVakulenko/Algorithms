e7ae463f8efa81c33e76f1d1c10472d4e26352ad
PetrVakulenko <vakulenkopetya@gmail.com>
2020-10-06 16:47:31 +0300

    Solution for `Count good triplets`

---
+++ src/Array/CountGoodTriplets_1534/Main.java
@@ -0,0 +1,12 @@
+package Array.CountGoodTriplets_1534;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] arr = {3,0,1,1,9,7};
+        int a = 7, b = 2, c = 3;
+
+        System.out.println(s.countGoodTriplets(arr, a, b, c));
+    }
+}
