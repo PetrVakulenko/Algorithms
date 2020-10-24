5872e6f86ebf476a0a993b8c08a3c506f20c125d
PetrVakulenko <vakulenkopetya@gmail.com>
2020-10-24 18:53:36 +0300

    Solution for `Three Consecutive odds`

---
+++ src/Array/ThreeConsecutiveOdds_1550/Main.java
@@ -0,0 +1,9 @@
+package Array.ThreeConsecutiveOdds_1550;
+
+public class Main {
+    public static void main(String[] argv){
+        Solution s = new Solution();
+        int[] matrix = {11,74,0,9340,11,74,7};
+        System.out.println(s.threeConsecutiveOdds(matrix));
+    }
+}
