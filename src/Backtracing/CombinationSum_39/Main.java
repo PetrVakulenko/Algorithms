de6b57c752c321e51ead2353a3efa9537a67e333
PetrVakulenko <vakulenkopetya@gmail.com>
2020-10-25 18:07:57 +0200

    Solution for `Combination sum`

---
+++ src/Backtracing/CombinationSum_39/Main.java
@@ -0,0 +1,10 @@
+package Backtracing.CombinationSum_39;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {2,3,5};
+
+        System.out.println(s.combinationSum(nums, 8));
+    }
+}
