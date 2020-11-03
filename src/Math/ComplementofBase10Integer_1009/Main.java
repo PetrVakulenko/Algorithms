bdb48d81acf13ed79d438736d3058eb68b37baf6
PetrVakulenko <vakulenkopetya@gmail.com>
2020-11-03 13:05:18 +0200

    Solution for `Complement of Base 10 Integer`

---
+++ src/Math/ComplementofBase10Integer_1009/Main.java
@@ -0,0 +1,9 @@
+package Math.ComplementofBase10Integer_1009;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int num = 6;
+        System.out.println(s.bitwiseComplement(num));
+    }
+}
