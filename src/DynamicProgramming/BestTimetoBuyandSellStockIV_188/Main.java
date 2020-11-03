95dc64719707c3571a52965f5e9c2428381853ab
PetrVakulenko <vakulenkopetya@gmail.com>
2020-11-03 12:50:16 +0200

    Solution for `Best time to buy and sell stock IV`

---
+++ src/DynamicProgramming/BestTimetoBuyandSellStockIV_188/Main.java
@@ -0,0 +1,9 @@
+package DynamicProgramming.BestTimetoBuyandSellStockIV_188;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] prices = {2,1,2,1,0,1,2};
+        System.out.println(s.maxProfit(2, prices));
+    }
+}
