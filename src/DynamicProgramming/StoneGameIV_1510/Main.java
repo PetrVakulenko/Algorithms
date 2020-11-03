590f2dfee8489277cc1395c9d81833c39561123e
PetrVakulenko <vakulenkopetya@gmail.com>
2020-11-03 12:37:08 +0200

    Solution for `Stone game 4`

---
+++ src/DynamicProgramming/StoneGameIV_1510/Main.java
@@ -0,0 +1,8 @@
+package DynamicProgramming.StoneGameIV_1510;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.winnerSquareGame(7));
+    }
+}
